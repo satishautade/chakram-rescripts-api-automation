196ebad11b961c87b7fadee3e07370b3e56757fb
Satish Autade <satisha@medadvisor.com.au>
2018-09-06 10:11:28 +1000

    Got the multiple script requests working. FIXED issues in HL7 formatting REFACTORED code as per Stephanie's comments

--- test/send_script_request_test.js
+++ test/send_script_request_test.js
@@ -6,11 +6,11 @@ var fs = require('fs');
 var helpers = require('../lib/helpers.js');
 
 describe("Create Script Request for GPConnect Doctor", function () {
-  let TEST_DATA;
-  let BASEURL = "https://t1.hnotes.com.au/rescripts8/services/";
+  let testData;
+  const BASEURL = "https://t1.hnotes.com.au/rescripts8/services/";
 
-  let USER_SESSION_BODY = fs.readFileSync('./data/user_session_body.xml', 'utf8');
-  let SCRIPT_REQUEST_BODY = fs.readFileSync('./data/script_request_body.xml', 'utf8');
+  const USER_SESSION_BODY = fs.readFileSync('./data/user_session_body.xml', 'utf8');
+  const SCRIPT_REQUEST_BODY = fs.readFileSync('./data/script_request_body.xml', 'utf8');
 
   let options = {
     headers: {
@@ -23,39 +23,36 @@ describe("Create Script Request for GPConnect Doctor", function () {
 
   this.beforeAll(() => {
 
-    const TEST_DATA_FILE = fs.readFileSync('./data/test_data.csv', 'utf8');
-    TEST_DATA = Papa.parse(TEST_DATA_FILE, {
+    const testDataFile = fs.readFileSync('./data/test_data.csv', 'utf8');
+    testData = Papa.parse(testDataFile, {
       //returns data with keys
       header: true
     }).data;
 
   });
 
-  it("should make a script request for each row of data using new session id", () => {
+  it("should make a script request for each row of data using new session id", async () => {
 
-    for(const dataRow of TEST_DATA){
+    for(const dataRow of testData){
 
-      console.log('**********************BEFORE*************************************');
-      console.log('DATA ROW => ' + JSON.stringify(dataRow));
-      return chakram.post(BASEURL + 'UserSessionService', eval(USER_SESSION_BODY), options)
+      options = helpers.setHeadersForUserSession(options);
+      await chakram.post(BASEURL + 'UserSessionService', eval(USER_SESSION_BODY), options)
         .then(function (userSessionResponse) {
           expect(userSessionResponse).to.have.status(200);
           var sessionId = helpers.parseSessionId(userSessionResponse);
-          console.log("shiny new session => " + sessionId);
-          options.headers["SOAPAction"] = "http://www.healthenterprises.com.au/rescripts/Script/sendRequest";
-          options.headers["Content-Type"] = "text/xml";
+          options = helpers.setHeadersForScriptRequest(options);
           return chakram.post(BASEURL + 'ScriptService', eval(SCRIPT_REQUEST_BODY), options)
         })
         .then(function (scriptRequestResponse) {
           expect(scriptRequestResponse).to.have.status(200);
           var referenceNo = helpers.parseReferenceNumber(scriptRequestResponse);
           console.log("Finally Our Reference no => " + referenceNo);
-          return chakram.wait();
         })
         .catch((err)=> {
-          console.log(err.toJSON());
           throw new Error(err.message);
         });
+      
     };
+    return chakram.wait();
   });
 });
